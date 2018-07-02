7f201571ceb788fb408dd100410b19a4de0daf26
yukihane <yukihane.feather@gmail.com>
2018-07-03 00:23:17 +0900

    role名の付け間違い

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ module.exports.loop = function () {
     const harvesters = room.find(FIND_MY_CREEPS)
       .filter((s) => s.memory["role"] === "harvester");
     if (harvesters.length < 1) {
+      console.log("spawn adam! " + harvesters.length);
       const spawns = room.find(FIND_MY_SPAWNS);
       if (spawns.length > 0) {
-        spawns[0].spawnCreep([WORK, MOVE, CARRY], "adam", { "role": "harvester" });
+        spawns[0].spawnCreep([WORK, MOVE, CARRY], "adam", {memory: { "role": "harvester" }});
+      }
+    } else {
+      console.log("go harvest!");
+      for(const harvester of harvesters) {
+        const source = harvester.pos.findClosestByPath(FIND_SOURCES);
+        console.log(source);
+        const ret = harvester.harvest(source);
+        if(ret == ERR_NOT_IN_RANGE) {
+          harvester.moveTo(source);
+        }
       }
     }
   }
